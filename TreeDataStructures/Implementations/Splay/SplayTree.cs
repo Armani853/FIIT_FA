c0cf12279de3c499c45acd48a87d47c2baedfcab
Armani <arman120906@mail.ru>
2026-03-31 17:59:23 +0300

    lab5 Splay done

--- TreeDataStructures/Implementations/Splay/SplayTree.cs
+++ TreeDataStructures/Implementations/Splay/SplayTree.cs
@@ -8,20 +8,113 @@ public class SplayTree<TKey, TValue> : BinarySearchTree<TKey, TValue>
 {
     protected override BstNode<TKey, TValue> CreateNode(TKey key, TValue value)
         => new(key, value);
+
+
+    private void Splay(BstNode<TKey, TValue>? node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+        while (node.Parent != null)
+        {
+            var parent = node.Parent;
+            var grandparent = parent.Parent;
+            if (grandparent == null)
+            {
+                if (node.IsLeftChild)
+                {
+                    base.RotateRight(parent);
+                } else
+                {
+                    base.RotateLeft(parent);
+                }
+            } else if (node.IsLeftChild && parent.IsLeftChild)
+            {
+                base.RotateRight(grandparent);
+                base.RotateRight(parent);
+            } else if (node.IsRightChild && parent.IsRightChild)
+            {
+                base.RotateLeft(grandparent);
+                base.RotateLeft(parent);
+            } else if (node.IsRightChild && parent.IsLeftChild)
+            {
+                base.RotateLeft(parent);
+                base.RotateRight(grandparent);
+            } else if (node.IsLeftChild && parent.IsRightChild)
+            {
+                base.RotateRight(parent);
+                base.RotateLeft(grandparent);
+            }
+        }
+        Root = node;
+        node.Parent = null;
+    }
     
     protected override void OnNodeAdded(BstNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        Splay(newNode);
     }
     
     protected override void OnNodeRemoved(BstNode<TKey, TValue>? parent, BstNode<TKey, TValue>? child)
     {
-        throw new NotImplementedException();
+        if (parent != null)
+        {
+            Splay(parent);
+        } else if (child != null)
+        {
+            Splay(child);
+        }
+    }
+
+    public override bool ContainsKey(TKey key)
+    {
+        var node = FindNode(key);
+        if (node != null)
+        {
+            Splay(node);
+            return true;
+        }
+        return false;
     }
     
     public override bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
     {
-        throw new NotImplementedException();
+        var node = FindNode(key);
+        if (node != null)
+        {
+            value = node.Value;
+            Splay(node);
+            return true;
+        }
+        if (Root != null)
+        {
+            var current = Root;
+            while (current != null)
+            {
+                int cmp = Comparer.Compare(key, current.Key);
+                if (cmp < 0)
+                {
+                    if (current.Left == null)
+                    {
+                        break;
+                    }
+                    current = current.Left;
+                } else if (cmp > 0)
+                {
+                    if (current.Right == null)
+                    {
+                        break;
+                    }
+                    current = current.Right;
+                } else
+                {
+                    break;
+                }
+            }
+            Splay(current);
+        }
+        value = default;
+        return false;
     }
-    
 }
